d8c0f41f5c3be5efae88f7358bb23cfa11794e20
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-04-03 15:59:29 +0100

    Finished base class for validation methods.

--- tsvalidation/validation_methods/_base.py
+++ tsvalidation/validation_methods/_base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Generator
 import numpy as np
 import pandas as pd
 
@@ -22,6 +23,12 @@ class base_splitter(ABC):
     _n_splits : int
         Number of splits for this instance of the validation method.
 
+    _ts : np.ndarray | pd.Series
+        Univariate time series.
+
+    _n_samples : int
+        Number of samples in the time series.
+
     Methods
     -------
     __init__(self, splits: int) -> None
@@ -30,20 +37,20 @@ class base_splitter(ABC):
     n_splits(self) -> int
         Returns the number of splits (set during initialisation).
 
-    split(self) -> tuple[list]
-
+    split(self) -> Generator[np.ndarray, np.ndarray]
+        Abstract method. The implementation differs for each validation method.
 
     info(self) -> None
-
+        Abstract method. The implementation differs for each validation method.
 
     statistics(self) -> None
-
+        Abstract method. The implementation differs for each validation method.
 
     plot(self) -> None
-
+        Abstract method. The implementation differs for each validation method.
     """
 
-    def __init__(self, splits: int) -> None:
+    def __init__(self, splits: int, ts: np.ndarray | pd.Series) -> None:
 
         """
         Class constructor.
@@ -57,9 +64,127 @@ class base_splitter(ABC):
         """
 
         super().__init__();
+        self._splits_check(splits);
+        self._ts_check(ts);
         self._n_splits = splits;
+        self._series = ts;
+        self._n_samples = self._series.shape[0];
+        self._dim_check();
     
         return;
+
+    def _splits_check(self, splits: int) -> None:
+        
+        """
+        Perform type and value checks on the 'splits' parameter.
+
+        This method checks whether 'splits' is an integer and whether 'splits' >= 2.
+
+        Parameters
+        ----------
+        splits : int
+            Number of splits.
+
+        Raises
+        ------
+        TypeError
+            If the number of splits is not an integer.
+
+        ValueError
+            If the number of splits is smaller than two.
+        """
+
+        if(isinstance(splits, int) is False):
+
+            raise TypeError("'splits' must be an integer.");
+    
+        elif(splits < 2):
+
+            raise ValueError("'splits' must be equal to or larger than two.");
+
+        return;
+
+    def _ts_check(self, ts: np.ndarray | pd.Series) -> None:
+        
+        """
+        Perform type and value checks on the 'ts' parameter.
+
+        This method checks whether 'ts' is a Numpy array or a Pandas series and whether
+        the time series is univariate.
+
+        Parameters
+        ----------
+        ts : np.ndarray | pd.Series
+            Univariate time series.
+
+        Raises
+        ------
+        TypeError
+            If ts is not a Numpy array nor a Pandas series.
+
+        ValueError
+            If the time series is multivariate (i.e. not one-dimensional).
+        """
+
+        if(isinstance(ts, np.ndarray) is False and isinstance(ts, pd.Series) is False):
+
+            raise TypeError("'ts' must be either a Numpy array or a Pandas series.");
+    
+        elif(len(ts.shape) > 1):
+                
+            raise ValueError("The time series must be univariate. Support for multivariate time series has yet to be implemented.\
+                              If the input is supposed to be a one-dimensional numpy array, consider flattening the array.");
+
+        return;
+
+    #def _ts_formatting(self, ts: np.ndarray | pd.Series) -> np.ndarray | pd.Series:
+
+        time_series = ts.copy();
+
+        if(isinstance(time_series, np.ndarray) and len(time_series.shape) == 2):
+
+            time_series = time_series.flatten();
+
+        return time_series;
+
+    def _dim_check(self) -> None:
+        
+        """
+        Perform a dimension check on the time series.
+
+        This method checks whether 'n_samples' >= 'splits'.
+
+        Raises
+        ------
+        ValueError
+            If the number of splits is larger than the number of samples in the time series.
+        """
+
+        if(self._n_splits > self._n_samples):
+
+            raise ValueError("The number of splits must be smaller than (or equal to) the number of samples in the time series.");
+
+        return;
+
+    # This should only be defined for prequential and CV methods!
+    #def _split_folds(self) -> list[np.ndarray]:
+
+        samples_per_fold = self._n_samples / self._n_splits;
+        extra_samples = self._n_samples // self._n_splits;
+
+        indices = np.arange(0, self._n_samples);
+        extra = np.zeros(self._n_splits);
+        extra[:extra_samples] = 1;
+        folds = [];
+        curr_ind = 0;
+
+        for split in range(self._n_splits):
+
+            next_ind = split*(samples_per_fold+1)+extra[split];
+            folds.append(indices[curr_ind:next_ind]);
+            curr_ind = next_ind;
+
+        return folds;
 
     @property
     def n_splits(self) -> int:
@@ -96,17 +221,17 @@ class base_splitter(ABC):
         return self._n_splits;
 
     @abstractmethod
-    def split(self) -> tuple[list]:
+    def split(self) -> Generator[np.ndarray, np.ndarray]:
         
         """
-        _summary_
+        Split the time series into training and validation sets.
 
-        _extended_summary_
+        Abstract method. The implementation differs for each validation method.
 
-        Returns
-        -------
-        tuple[list]
-            _description_
+        Yields
+        ------
+        Generator[np.ndarray, np.ndarray]
+            Numpy arrays containing the training and validation indices.
         """
 
         pass
@@ -115,9 +240,9 @@ class base_splitter(ABC):
     def info(self) -> None:
         
         """
-        _summary_
+        Provide additional information on the validation method.
 
-        _extended_summary_
+        Abstract method. The implementation differs for each validation method.
         """
 
         pass
@@ -126,9 +251,9 @@ class base_splitter(ABC):
     def statistics(self) -> None:
         
         """
-        _summary_
+        Compute and plot relevant statistics for both training and validation sets.
 
-        _extended_summary_
+        Abstract method. The implementation differs for each validation method.
         """
 
         pass
@@ -137,9 +262,9 @@ class base_splitter(ABC):
     def plot(self) -> None:
         
         """
-        _summary_
+        Plot the partitioned time series.
 
-        _extended_summary_
+        Abstract method. The implementation differs for each validation method.
         """
 
         pass
