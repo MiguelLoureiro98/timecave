dc12d86abde3927f854d5f8a7f67672c475d99f2
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-04-10 23:46:56 +0100

    Started data characteristics wrapper functions.

---
+++ tsvalidation/data_characteristics.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import tsfel
+
+def statistical_features(ts: np.ndarray | pd.Series) -> dict:
+
+    pass
+
+def frequency_features(ts: np.ndarray | pd.Series) -> dict:
+
+    pass
+
+def all_features(ts: np.ndarray | pd.Series) -> dict:
+
+    pass
