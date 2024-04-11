a040411cc6122f5fe388ec3ad68fa252d93e10f7
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-04-11 18:05:38 +0100

    Added utils.

---
+++ tsvalidation/utils.py
@@ -0,0 +1,15 @@
+"""
+This module contains utility functions to help the user.
+"""
+
+def Nyquist_min_samples(fs: float | int, freq_limit: float | int) -> int:
+
+    pass
+
+def heuristic_min_samples(fs: float | int, freq_limit: float | int) -> int:
+
+    pass
+
+def true_test_indices(test_ind: list, model_order: int) -> list:
+
+    raise NotImplementedError("Not implemented yet.");
