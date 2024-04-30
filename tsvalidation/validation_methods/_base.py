2919c9ee70b200c3cfaff396af69993edd2196dd
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-04-30 15:05:14 +0100

    Formatted base class using Black.

--- tsvalidation/validation_methods/_base.py
+++ tsvalidation/validation_methods/_base.py
@@ -16,8 +16,8 @@ import pandas as pd
 # This should work with sklearn's Hyperparameter Search algorithms. If not, install sklearn and inherit from the BaseCrossValidator class (maybe?).
 # For now, leave it as it is, as this approach will most likely work with said search algorithms and leads to fewer requirements.
 
+
 class base_splitter(ABC):
-    
     """
     Base class for all time series validation methods provided / supported by this package.
 
@@ -67,8 +67,9 @@ class base_splitter(ABC):
         Abstract method. The implementation differs for each validation method.
     """
 
-    def __init__(self, splits: int, ts: np.ndarray | pd.Series, fs: float | int) -> None:
-
+    def __init__(
+        self, splits: int, ts: np.ndarray | pd.Series, fs: float | int
+    ) -> None:
         """
         Class constructor.
 
@@ -86,21 +87,20 @@ class base_splitter(ABC):
             The series' sampling frequency (Hz).
         """
 
-        super().__init__();
-        self._splits_check(splits);
-        self._ts_check(ts);
-        self._fs_check(fs);
-        self._n_splits = splits;
-        self._series = ts;
-        self._fs = fs;
-        self._n_samples = self._series.shape[0];
-        self._dim_check();
-        self._indices = np.arange(0, self._n_samples);
-    
-        return;
+        super().__init__()
+        self._splits_check(splits)
+        self._ts_check(ts)
+        self._fs_check(fs)
+        self._n_splits = splits
+        self._series = ts
+        self._fs = fs
+        self._n_samples = self._series.shape[0]
+        self._dim_check()
+        self._indices = np.arange(0, self._n_samples)
+
+        return
 
     def _splits_check(self, splits: int) -> None:
-        
         """
         Perform type and value checks on the 'splits' parameter.
 
@@ -120,18 +120,17 @@ class base_splitter(ABC):
             If the number of splits is smaller than two.
         """
 
-        if(isinstance(splits, int) is False):
+        if isinstance(splits, int) is False:
 
-            raise TypeError("'splits' must be an integer.");
-    
-        elif(splits < 2):
+            raise TypeError("'splits' must be an integer.")
 
-            raise ValueError("'splits' must be equal to or larger than two.");
+        elif splits < 2:
 
-        return;
+            raise ValueError("'splits' must be equal to or larger than two.")
+
+        return
 
     def _ts_check(self, ts: np.ndarray | pd.Series) -> None:
-        
         """
         Perform type and value checks on the 'ts' parameter.
 
@@ -152,29 +151,30 @@ class base_splitter(ABC):
             If the time series is multivariate (i.e. not one-dimensional).
         """
 
-        if(isinstance(ts, np.ndarray) is False and isinstance(ts, pd.Series) is False):
+        if isinstance(ts, np.ndarray) is False and isinstance(ts, pd.Series) is False:
 
-            raise TypeError("'ts' must be either a Numpy array or a Pandas series.");
-    
-        elif(len(ts.shape) > 1):
-                
-            raise ValueError("The time series must be univariate. Support for multivariate time series has yet to be implemented.\
-                              If the input is supposed to be a one-dimensional numpy array, consider flattening the array.");
+            raise TypeError("'ts' must be either a Numpy array or a Pandas series.")
 
-        return;
+        elif len(ts.shape) > 1:
 
-    #def _ts_formatting(self, ts: np.ndarray | pd.Series) -> np.ndarray | pd.Series:
+            raise ValueError(
+                "The time series must be univariate. Support for multivariate time series has yet to be implemented.\
+                              If the input is supposed to be a one-dimensional numpy array, consider flattening the array."
+            )
 
-        time_series = ts.copy();
+        return
 
-        if(isinstance(time_series, np.ndarray) and len(time_series.shape) == 2):
+        # def _ts_formatting(self, ts: np.ndarray | pd.Series) -> np.ndarray | pd.Series:
 
-            time_series = time_series.flatten();
+        time_series = ts.copy()
 
-        return time_series;
+        if isinstance(time_series, np.ndarray) and len(time_series.shape) == 2:
+
+            time_series = time_series.flatten()
+
+        return time_series
 
     def _dim_check(self) -> None:
-        
         """
         Perform a dimension check on the time series.
 
@@ -186,57 +186,59 @@ class base_splitter(ABC):
             If the number of splits is larger than the number of samples in the time series.
         """
 
-        if(self._n_splits > self._n_samples):
+        if self._n_splits > self._n_samples:
 
-            raise ValueError("The number of splits must be smaller than (or equal to) the number of samples in the time series.");
+            raise ValueError(
+                "The number of splits must be smaller than (or equal to) the number of samples in the time series."
+            )
 
-        return;
+        return
 
     def _fs_check(self, fs: int | float) -> None:
-
         """
         Perform type and value checks on the series' sampling frequency.
         """
 
-        if((isinstance(fs, float) or isinstance(fs, int)) is False):
+        if (isinstance(fs, float) or isinstance(fs, int)) is False:
 
-            raise TypeError("The sampling frequency should be either a float or an integer.");
+            raise TypeError(
+                "The sampling frequency should be either a float or an integer."
+            )
 
-        if(fs <= 0):
+        if fs <= 0:
 
-            raise ValueError("The sampling frequency should be larger than zero.");
+            raise ValueError("The sampling frequency should be larger than zero.")
 
-        return;
+        return
 
-    # This should only be defined for prequential and CV methods!
-    #def _split_folds(self) -> list[np.ndarray]:
+        # This should only be defined for prequential and CV methods!
+        # def _split_folds(self) -> list[np.ndarray]:
 
-        samples_per_fold = self._n_samples / self._n_splits;
-        extra_samples = self._n_samples // self._n_splits;
+        samples_per_fold = self._n_samples / self._n_splits
+        extra_samples = self._n_samples // self._n_splits
 
-        indices = np.arange(0, self._n_samples);
-        extra = np.zeros(self._n_splits);
-        extra[:extra_samples] = 1;
-        folds = [];
-        curr_ind = 0;
+        indices = np.arange(0, self._n_samples)
+        extra = np.zeros(self._n_splits)
+        extra[:extra_samples] = 1
+        folds = []
+        curr_ind = 0
 
         for split in range(self._n_splits):
 
-            next_ind = split*(samples_per_fold+1)+extra[split];
-            folds.append(indices[curr_ind:next_ind]);
-            curr_ind = next_ind;
+            next_ind = split * (samples_per_fold + 1) + extra[split]
+            folds.append(indices[curr_ind:next_ind])
+            curr_ind = next_ind
 
-        return folds;
+        return folds
 
     @property
     def n_splits(self) -> int:
-        
         """
         Get the number of splits for a given instance of a validation method.
 
-        This method can be used to retrieve the number of splits for a given 
+        This method can be used to retrieve the number of splits for a given
         instance of a validation method (this is set on initialisation).
-        Since the method is implemented as a property, this information can 
+        Since the method is implemented as a property, this information can
         simply be accessed as an attribute using dot notation.
 
         Returns
@@ -245,18 +247,17 @@ class base_splitter(ABC):
             The number of splits.
         """
 
-        return self._n_splits;
-    
+        return self._n_splits
+
     @property
     def sampling_freq(self) -> int | float:
-
         """
         Get the time series' sampling frequency.
 
         This method can be used to access the time series' sampling
         frequency, in Hertz (this is set on intialisation).
-        Since the method is implemented as a property, this information can 
-        simply be accessed as an attribute using dot notation. 
+        Since the method is implemented as a property, this information can
+        simply be accessed as an attribute using dot notation.
 
         Returns
         -------
@@ -264,12 +265,12 @@ class base_splitter(ABC):
             The time series' sampling frequency (Hz).
         """
 
-        return self._fs;
+        return self._fs
 
-    # If the sklearn-like interface is to be kept, then this version of the 'get_n_splits' should be implemented.
+        # If the sklearn-like interface is to be kept, then this version of the 'get_n_splits' should be implemented.
 
-    #def get_n_splits(self) -> int:
-        
+        # def get_n_splits(self) -> int:
+
         """
         Get the number of splits for a given instance of a validation method.
 
@@ -281,11 +282,10 @@ class base_splitter(ABC):
             The number of splits.
         """
 
-        return self._n_splits;
+        return self._n_splits
 
     @abstractmethod
     def split(self) -> Generator[tuple, None, None]:
-        
         """
         Split the time series into training and validation sets.
 
@@ -301,7 +301,6 @@ class base_splitter(ABC):
 
     @abstractmethod
     def info(self) -> None:
-        
         """
         Provide additional information on the validation method.
 
@@ -312,7 +311,6 @@ class base_splitter(ABC):
 
     @abstractmethod
     def statistics(self) -> tuple[pd.DataFrame]:
-        
         """
         Compute and plot relevant statistics for both training and validation sets.
 
@@ -344,26 +342,31 @@ class base_splitter(ABC):
 
         pass
 
+
 if __name__ == "__main__":
 
-    def split(X: np.ndarray, splits: int=5):
+    def split(X: np.ndarray, splits: int = 5):
 
-        samples = X.shape[0];
-        indices = np.arange(0, samples);
-        samples_per_index = int(np.round(samples / splits));
+        samples = X.shape[0]
+        indices = np.arange(0, samples)
+        samples_per_index = int(np.round(samples / splits))
 
-        for split in range(splits-1):
+        for split in range(splits - 1):
 
-            train_ind = indices[split * samples_per_index:(split + 1) * samples_per_index];
-            test_ind = indices[train_ind + np.fmin(samples_per_index, samples - train_ind[-1])];
+            train_ind = indices[
+                split * samples_per_index : (split + 1) * samples_per_index
+            ]
+            test_ind = indices[
+                train_ind + np.fmin(samples_per_index, samples - train_ind[-1])
+            ]
 
-            yield train_ind, test_ind;
+            yield train_ind, test_ind
 
-    X = np.ones(10);
+    X = np.ones(10)
 
     for train, test in split(X):
 
-        print(train);
-        print(test);
+        print(train)
+        print(test)
 
-    print(np.arange(13-5*2, 13, 2));
+    print(np.arange(13 - 5 * 2, 13, 2))
