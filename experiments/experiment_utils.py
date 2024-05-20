311c88361a17ca80b246e0b23b1af7a0902bbd8d
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-05-20 14:08:49 +0100

    Added utility functions.

---
+++ experiments/experiment_utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+def get_univariate_series(dataset: pd.DataFrame) -> list[pd.Series]:
+
+    """
+    Split a multivariate time series into several univariate ones.
+    """
+
+    series_list = [dataset[column].copy() for column in dataset.columns[1:]];
+
+    return series_list;
+
+def split_series(ts: pd.Series, test_set_proportion: float=0.2) -> tuple[pd.Series]:
+
+    """
+    Split a univariate time series into training and test sets.
+    """
+
+    splitting_index = np.round((1 - test_set_proportion) * ts.shape[0]);
+
+    train = ts.iloc[:splitting_index];
+    test = ts.iloc[splitting_index:];
+
+    return (train, test);
+
+def shape_series(ts: pd.Series, n_lags: int=5) -> pd.DataFrame:
+
+    """
+    Format a time series so it can be fed to an LSTM and a Decision Tree.
+    """
+
+    lags = [ts.shift(lag) for lag in range(1, n_lags + 1)];
+    col_names = ["t + {}".format(i) for i in range(n_lags + 1)];
+
+    lags = pd.concat(lags, axis=1);
+    series = pd.concat([ts, lags], axis=1);
+    series.columns = col_names;
+    series = series.dropna(how="all");
+
+    return series;
