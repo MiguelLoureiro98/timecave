3a7b21290f359224bca7ff413672843b30d7e273
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-05-22 14:26:19 +0100

    Finished DT & ARIMA.

--- experiments/models.py
+++ experiments/models.py
@@ -3,40 +3,40 @@ import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from statsmodels.tsa.arima.model import ARIMA
-from experiment_utils import shape_series, get_X_y
+from experiment_utils import get_X_y
 
-def recursive_forecast_tree(ts_val: np.ndarray, n_lags: int, pred_window: int, model: DecisionTreeRegressor) -> np.ndarray:
+def recursive_forecast_tree(ts_val: np.ndarray, pred_window: int, model: DecisionTreeRegressor) -> np.ndarray:
 
     """
     Recursive forecasting for decision trees.
     """
 
     forecasts = np.zeros(pred_window);
-    input = ts_val[:n_lags];
+    input = ts_val[0];
 
     for ind in range(pred_window):
 
-        forecasts[ind] = model.predict(input);
+        forecasts[ind] = model.predict(input.reshape(1, -1)).item();
         input = np.hstack((input[1:], forecasts[ind]));
 
     return forecasts;
 
-def predict_tree(ts_train: pd.Series | np.ndarray, ts_val: pd.Series | np.ndarray, n_lags: int) -> dict:
+def predict_tree(ts_train: pd.Series, ts_val: pd.Series) -> dict:
     
     """
     Train and test a decision tree model.
     """
 
-    reshaped_train = shape_series(ts_train);
-    reshaped_val = shape_series(ts_val);
-    X_train, y_train = get_X_y(reshaped_train);
-    X_val, y_val = get_X_y(reshaped_val);
+    #reshaped_train = shape_series(ts_train);
+    #reshaped_val = shape_series(ts_val);
+    X_train, y_train = get_X_y(ts_train);
+    X_val, y_val = get_X_y(ts_val);
 
     model = DecisionTreeRegressor();
 
     model.fit(X_train, y_train);
 
-    y_pred = recursive_forecast_tree(X_val, n_lags, y_val.shape[0], model);
+    y_pred = recursive_forecast_tree(X_val, y_val.shape[0], model);
     mse = mean_squared_error(y_true=y_val, y_pred=y_pred);
     rmse = np.sqrt(mse);
     mae = mean_absolute_error(y_true=y_val, y_pred=y_pred);
@@ -76,15 +76,16 @@ if __name__ == "__main__":
     a = np.ones(100);
     b = np.ones(10);
 
-    #model = ARIMA(a, order=(5, 5, 0));
-    #res = model.fit();
+    a = np.append(np.arange(100), np.arange(100, 120));
+    b = np.arange(100, 110, 1);
 
-    #print(res.forecast(10));
+    ARMA_results = predict_ARMA(a, b);
 
-    #ARMA_results = predict_ARMA(a, b);
+    print(ARMA_results);
 
-    #print(ARMA_results);
+    a = pd.Series(a);
+    b = pd.Series(b);
 
-    DT_res = predict_tree(a, b, 5);
+    DT_res = predict_tree(a, b);
 
     print(DT_res);
