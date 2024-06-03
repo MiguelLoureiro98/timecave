d068f29d500668b8ad121dca865c47783fc18a42
BeatrizPLourenco <beatriz.p.lourenco@tecnico.ulisboa.pt>
2024-06-03 13:00:21 +0100

    changed dir txt

--- experiments/transportes.py
+++ experiments/transportes.py
@@ -3,9 +3,9 @@ from run_experiments import run
 backup_dir = "results/backups"
 results_dir = "results"
 transportes = [
-    "datasets\\processed_data\\metro_nyc_17052024.csv",
-    "datasets\\processed_data\\taxi_data_15052024.csv",
-    "datasets\\processed_data\\traffic_17052024.csv",
+    "datasets/processed_data/metro_nyc_17052024.csv",
+    "datasets/processed_data/taxi_data_15052024.csv",
+    "datasets/processed_data/traffic_17052024.csv",
 ]
 
 run(transportes, backup_dir, results_dir, resume_run=False, add_name="transportes")
