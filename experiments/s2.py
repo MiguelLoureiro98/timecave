e246d51938c1bbc996b3c5ddeeb7b83d31388d66
BeatrizPLourenco <beatriz.p.lourenco@tecnico.ulisboa.pt>
2024-06-07 13:01:40 +0100

    results synthetic s2 (200_1001)

--- experiments/s2.py
+++ experiments/s2.py
@@ -12,7 +12,7 @@ run(
     backup_dir,
     results_dir,
     resume_run=False,
-    add_name="s2_200_1000",
+    add_name="s2_200_1001",
     models=["ARMA", "tree"],
     save_stats=False,
     from_ts=200,
