44a61409e4830a12a12919e3b6570a4f9946913f
Beatriz - Colab <beatriz.plourenco99@gmail.com>
2024-07-23 13:31:40 +0100

    test_job.sh modified

--- test_job.sh
+++ test_job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=test_job
-#SBATCH --time=0:10:00
+#SBATCH --job-name=Try
+#SBATCH --time=0:01:00
 #SBATCH --partition=hpc
 #SBATCH --error=err.job.%j
-#SBATCH -D
+#SBATCH --output=out.job.%j
+#SBATCH -D .
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=2
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
@@ -15,14 +16,18 @@ module load python/3.10.13
 module load cuda/12.1
 module load tensorflow/2.14-0
 
-git pull origin main
 git checkout Colab_Beatriz
-git merge main
-git commit -m "get from main"
-git push origin Colab_Beatriz
-python experiments/test_hpc.py
-git add experiments/results/tests
-git commit -m "test results from hpc."
-git push origin Colab_Beatriz
+git fetch origin
 
+# Check if the branch is up to date with main
+if ! git diff --quiet origin/main; then
+  echo "Branch is not up to date with main. Merging main into Colab_Beatriz."
+  git merge origin/main
+fi
+python experiments/test_hpc.py
+git config --global user.email "beatriz.plourenco99@gmail.com"
+git config --global user.name "Beatriz - Colab"
+git add experiments/results/tests
+git commit -m "test results from hpc 2."
+git push origin Colab_Beatriz
 exit
