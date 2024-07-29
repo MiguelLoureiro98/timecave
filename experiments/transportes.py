545431d7218ac5bdbef2d3dc56492865c60684c0
Miguel Santos Loureiro <miguel.santos.loureiro@gmail.com>
2024-07-29 14:26:27 +0100

    job attempt number 126753176538127392738172

--- experiments/transportes.py
+++ experiments/transportes.py
@@ -11,35 +11,6 @@ transportes = [
     "datasets/processed_data/traffic_17052024.csv",
 ]
 
-commands = [
-    "git checkout transportes",
-    "git fetch origin",
-    "if ! git diff --quiet origin/main; then git merge origin/main fi",
-    'if ! git diff --quiet origin/main; then git config --global user.email "beatriz.plourenco99@gmail.com" fi',
-    'if ! git diff --quiet origin/main; then git config --global user.name "Beatriz - Colab" fi',
-    "if ! git diff --quiet origin/main; then git add .  fi",
-    'if ! git diff --quiet origin/main; then git commit -m "Merge"  fi',
-    "if ! git diff --quiet origin/main; then git push origin transportes  fi",
-]
-
-full_command = " && ".join(commands)
-
-
-result = subprocess.run(full_command, shell=True, check=True, text=True, capture_output=True)
-
-print("Return code:", result.returncode)
-print("Standard Output:", result.stdout)
-print("Standard Error:", result.stderr)
-
-
-cmd = """
-git config --global user.email "beatriz.plourenco99@gmail.com"
-git config --global user.name "Beatriz - Colab"
-git add experiments/results/transportes
-git commit -m "transportes results from hpc."
-git push origin transportes
-"""
-
 run(
     transportes,
     backup_dir,
@@ -47,6 +18,5 @@ run(
     resume_run=False,
     add_name="transportes_LSTM",
     models=["LSTM"],
-    save_stats=False,
-    cmd=cmd
+    save_stats=False
 )
